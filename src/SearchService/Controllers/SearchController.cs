a725515b87e70e7e02861122e0cd8c561819352a
dulsaranethmin <dulsaranethmin@gmail.com>
2024-04-13 05:08:39 +0530

    search service

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams) 
+        {
+            var query = DB.PagedSearch<Item, Item>();
+
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+            {
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+            }
+
+            query = searchParams.OrderBy switch
+            {
+                "new" => query.Sort(a => a.CreatedAt, Order.Ascending),
+                "old" => query.Sort(a => a.CreatedAt, Order.Descending),
+                "make" => query.Sort(a => a.Make, Order.Ascending),
+                _ => query.Sort(a => a.AuctionEnd, Order.Ascending),
+            };
+
+            query = searchParams.FilterBy switch
+            {
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+                
+            };
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(a => a.Seller == searchParams.Seller);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(a => a.Winner == searchParams.Winner);
+            }
+
+            
+
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
+
+            var result = await query.ExecuteAsync();
+
+            return Ok(new
+            {
+                results = result.Results,
+                totalCount = result.TotalCount,
+                pageCount = result.PageCount,
+            });
+               
+        }
+    }
+}
---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using System.Text.Json;
+
+namespace SearchService.Data
+{
+    public class DbInitializer
+    {
+        public static async Task InitDb(WebApplication app) 
+        {
+            await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoConnectionString")));
+
+            await DB.Index<Item>()
+                .Key(a => a.Make, KeyType.Text)
+                .Key(a => a.Model, KeyType.Text)
+                .Key(a => a.Color, KeyType.Text)
+                .CreateAsync();
+
+            var count = await DB.CountAsync<Item>();
+
+            if (count == 0)
+            {
+                Console.WriteLine("Seeding data...");   
+                // seed data
+
+                var dataitems = await File.ReadAllTextAsync("Data/ItemData.json");
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+
+                var items = JsonSerializer.Deserialize<List<Item>>(dataitems, options);
+
+                await DB.SaveAsync(items);
+            }
+        }
+
+    }
+}
